869909c78e04764ffaf7958fa62733a30315c314
Shivansh Murjani <murjani.vmware@gmail.com>
2023-08-07 11:01:35 +0530

    connect buttons to function

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const divContain = document.querySelector('#divContainer');
 
 
 
-function createDiv(number){
+function createDiv(number,x,y){
     for(let i=0;i<number;i++){
     var newDiv = document.createElement('div');
+    newDiv.setAttribute('style','width:'+x+'px; height:'+y+'px;');
     divContain.appendChild(newDiv);
 }
 }
@@ -12,39 +13,37 @@ function createDiv(number){
 function addDiv(number){
     switch(number){
         case 16:
-            createDiv(1);
+            createDiv(1,600,600);
             //set style attributes
+
             break;
         
         case 32:
-            createDiv(36);
+            createDiv(36,100,100);
+
             break;
         case 48:
-            createDiv(4);
+            createDiv(48);
             //set style attributes
             break;
         
         case 64:
-            createDiv(8);
+            createDiv(64);
             break;       
         case 80:
-            createDiv(16);
+            createDiv(80);
             //set style attributes
             break;
         
         case 96:
-            createDiv(32);
+            createDiv(96);
             break;
-        case 64:
-            createDiv(64);
-            //set style attributes
-            break;
-        
         case 112:
-            createDiv(128);
+            createDiv(112);
             break;  
+
         case 128:
-            createDiv(144);
+            createDiv(128);
             break;    
 
     }
